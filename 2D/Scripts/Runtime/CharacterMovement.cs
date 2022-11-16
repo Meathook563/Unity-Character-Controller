f21f923c042950eeb75ea3a61f68ded289fc6ab7
Meathook563 <78488173+Meathook563@users.noreply.github.com>
2022-11-16 15:44:47 +0000

    Fixed 2D Scripts

--- 2D/Scripts/Runtime/CharacterMovement.cs
+++ 2D/Scripts/Runtime/CharacterMovement.cs
@@ -4,22 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Bus
+
+public class CharacterMovement : MonoBehaviour
 {
-    public class CharacterMovement : MonoBehaviour
-    {
-        [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 5f;
         
-        private Vector2 _move;
+    private Vector2 _move;
 
-        public void OnMove(InputAction.CallbackContext context)
-        {
-            _move = context.ReadValue<Vector2>();
-        }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _move = context.ReadValue<Vector2>();
+    }
 
-        private void Update()
-        {
-            transform.Translate(_move * moveSpeed * Time.deltaTime, Space.World);
-        }
+    private void Update()
+    {
+        transform.Translate(_move * moveSpeed * Time.deltaTime, Space.World);
     }
 }
+
